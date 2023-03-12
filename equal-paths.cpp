70c8453b477f01f261f4135c030a8a264bd6154d
Anish Jayant <jayant@usc.edu>
2023-03-12 21:25:57 +0000

    equal-paths done

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,5 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
+#include <algorithm>
 
 #endif
 
@@ -10,9 +11,27 @@ using namespace std;
 // You may add any prototypes of helper functions here
 
 
+int height(Node * root)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    return 1 + std::max(height(root->left), height(root->right));
+}
+
+bool isLeaf(Node* root)
+{
+    return (root != NULL) && (!root->left && !root->right);
+}
+
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+    if (root == NULL) return true;
+    else if ((isLeaf(root->left) && !root->right) || (isLeaf(root->right) && !root->left)) return true;
+    return (height(root->left) == height(root->right));
 }
+
+
 
